1b8e5946bae1b6ae77123512f6af7295a741c7e7
Agile Tross <scott.jackson.swe@gmail.com>
2017-12-27 22:52:12 -0700

    Transaction keys setup.

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import csv
 
 print("Starting the Tasty Trade Script")
 
-transaction_hash = []
+transaction_map = {}
 
 with open('real_data/2017_real_master.csv', newline='') as csvfile:
      spamreader = csv.DictReader(csvfile)
      for row in spamreader:
-         #print(', '.join(row))
-         print(row['Date'], row['Symbol'])
+         key = row['Symbol'].split(" ")[0]
+         if key in transaction_map:
+            transaction_map[key].append(row['Date'])
+         else:
+            transaction_map[key] = []
+            transaction_map[key].append(row['Date'])
+
+
+print ("Now Diplaying Transaciton Hash...")
+for k,v in transaction_map.items():
+     print(k,':',v)
+
+
 print("Exiting")
